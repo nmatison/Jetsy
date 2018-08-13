76152544981a620636e92fc58f167ca36f4fb148
Nick <nmatison@gmail.com>
2018-08-13 16:48:00 -0700

    Fix s3 bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.active_storage.service = :amazon_dev
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+  config.active_storage.service = :amazon_dev
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
