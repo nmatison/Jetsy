1c460208f7043114b1af0c23b9efe9bf47aa3fa9
Nick <nmatison@gmail.com>
2018-08-13 22:06:47 -0700

    Finish the bottom two divs on the splash page

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import SplashPageItems from './splash_page_items';
 
 const splashPage = () => {
   return (
     <div className="splash-page">
       <div className="splash-main">
         <div className="slide-show-ad">
-          <div className="slideshow">Slide Show</div>
+          <div className="slideshow"></div>
           <div className="advertisement">
             <div className="ad-image"></div>
             <div className="ad-div">
@@ -13,7 +14,31 @@ const splashPage = () => {
             </div>
           </div>
         </div>
-        <div className="our-picks"></div>
+        <div className="safe-jetsy">
+          <div className="safe"></div>
+          <div className="slogan-divs">
+            <h1 className="safe-header">Jetsy keeps you safe</h1>
+            <ul className="slogan-subdivs">
+              <li className="slogan-subdiv">
+                <h1 className="sub-div-head">Trustworthy sellers</h1>
+                <p className="sub-div-text">You can read their reviews placed by other consumers like you.</p>
+            </li>
+              <li className="slogan-subdiv">
+                <h1 className="sub-div-head">World-class security</h1>
+                <p className="sub-div-text">Safeguarding your information is the top priority of our dedicated security experts</p>
+            </li>
+              <li className="slogan-subdiv">
+                <h1 className="sub-div-head">Purchase protection</h1>
+                <p className="sub-div-text">If anything goes wrong, our global support team has got your back</p>
+            </li>
+            </ul>
+          </div>
+          <div className="keys"></div>
+        </div>
+        <div className="our-picks">
+          <h1 className="favorites">Our Current Favorites:</h1>
+          <SplashPageItems />
+        </div>
       </div>
     </div>
   )
---
+++ frontend/components/splash/splash_page_items.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SplashPageItems = () => {
+  return (
+    <ul className="splash-page-items">
+      <li className="splash-item">
+      </li>
+      <li className="splash-item">
+      </li>
+      <li className="splash-item">
+      </li>
+      <li className="splash-item">
+      </li>
+      <li className="splash-item">
+      </li>
+    </ul>
+  )
+}
+
+
+export default SplashPageItems;
