ea689033a6272939b54233789deb3206a9b5b7c1
Nick <nmatison@gmail.com>
2018-09-16 21:49:10 -0700

    Refactor layout for cart checkout

--- frontend/components/cart/cart.jsx
+++ frontend/components/cart/cart.jsx
@@ -41,44 +41,45 @@ class Cart extends React.Component {
   render() {
     if (!this.props.cartItems) return null
 
-    return (
-    
-      <div className="cart-main">
-          <h1>Shopping Cart</h1>
-      <div className="cart-container">
-        <div className="cart-items">
-          <div className="cart-item-list">
-            {this.cartList(
-              this.props.cartItems,
-              this.props.cartProducts,
-              this.props.deleteCartItem
-            )}
+    return <div className="cart-main">
+        <h1>Shopping Cart</h1>
+        <div className="cart-container">
+          <div className="cart-items">
+            <div className="cart-item-list">
+              {this.cartList(
+                this.props.cartItems,
+                this.props.cartProducts,
+                this.props.deleteCartItem
+              )}
+            </div>
           </div>
-        </div>
-        <div className="cart-total">
-          <div className="cart-total-information">
-            <ul className="cart-total-left">
-              <li>{`Total Items:`}</li>
-              <li>{`Subtotal:`}</li>
-              <li>{`Tax:`}</li>
-              <li >{`Shipping:`}</li>
-              <li>{`Estimated Total:`}</li>
-            </ul>
-            <ul className="cart-total-right">
-              <li>{`${this.totalItems(this.props.cartItems)}`}</li>
-              <li>{`$${this.calcTotal(this.props.cartItems, this.props.cartProducts)}`}</li>
-              <li>{`--`}</li>
-              <li>{`free`}</li>
-              <li>{`$${this.calcTotal(this.props.cartItems, this.props.cartProducts)}`}</li>
-            </ul>
+          <div className="cart-total">
+          <div className="cart-total-container">
+            <div className="cart-total-information">
+              <ul className="cart-total-left">
+                <li>{`Subtotal:`}</li>
+                <li>{`Total Items:`}</li>
+                <li>{`Tax:`}</li>
+                <li>{`Shipping:`}</li>
+              </ul>
+              <ul className="cart-total-right">
+                <li>{`$${this.calcTotal(this.props.cartItems, this.props.cartProducts)}`}</li>
+                <li className="">{`${this.totalItems(this.props.cartItems)}`}</li>
+                <li>{`--`}</li>
+                <li>{`free`}</li>
+              </ul>
+            </div>
+              <div className="estimated-total">
+                <li>{`Total:`}</li>
+                <li>{`$${this.calcTotal(this.props.cartItems, this.props.cartProducts)}`}</li>
+              </div>
+            <div className="checkout-button">
+              <button>Checkout</button>
+            </div>
           </div>
-          <div className="checkout-button">
-            <button>Checkout</button>
-          </div>
-        </div>
         </div>
       </div>
-    );
+      </div>;
   }
 }
 
