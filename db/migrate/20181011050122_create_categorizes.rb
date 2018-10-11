095f50578312baf334bf01088546f2c90de433a0
Nick <nmatison@gmail.com>
2018-10-10 22:04:40 -0700

    Fix issues with migration

--- db/migrate/20181011050122_create_categorizes.rb
+++ db/migrate/20181011050122_create_categorizes.rb
@@ -1,8 +1,8 @@
 class CreateCategorizes < ActiveRecord::Migration[5.2]
   def change
     create_table :categorizes do |t|
-      t.integer: :product_id, null: false
-      t.integer: :category_id, null: false 
+      t.integer :product_id, null: false
+      t.integer :category_id, null: false 
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_155628) do
+ActiveRecord::Schema.define(version: 2018_10_11_050122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,22 @@ ActiveRecord::Schema.define(version: 2018_08_16_155628) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_name"], name: "index_categories_on_category_name", unique: true
+  end
+
+  create_table "categorizes", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categorizes_on_category_id"
+    t.index ["product_id"], name: "index_categorizes_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
