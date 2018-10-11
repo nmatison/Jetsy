a3d2acf145351cceba41cbcf7b4de2ccd75e4548
Nick <nmatison@gmail.com>
2018-10-10 22:02:54 -0700

    Create categories and categorizes tables

---
+++ db/migrate/20181011050122_create_categorizes.rb
@@ -0,0 +1,12 @@
+class CreateCategorizes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categorizes do |t|
+      t.integer: :product_id, null: false
+      t.integer: :category_id, null: false 
+
+      t.timestamps
+    end
+    add_index :categorizes, :product_id
+    add_index :categorizes, :category_id
+  end
+end
